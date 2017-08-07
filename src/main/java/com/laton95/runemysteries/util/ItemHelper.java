cb508c831c662ab9e0c6d3a41d82b6ad1deda4ff
Hayden <haydenthomas6075@gmail.com>
2017-08-07 23:51:17 +1000

    Added item pickup sound to rune bag auto-collect

--- src/main/java/com/laton95/runemysteries/util/ItemHelper.java
+++ src/main/java/com/laton95/runemysteries/util/ItemHelper.java
@@ -1,5 +1,7 @@
 package com.laton95.runemysteries.util;
 
+import java.util.Random;
+
 import com.laton95.runemysteries.inventory.InventoryRuneBag;
 import com.laton95.runemysteries.item.ItemRune;
 import com.laton95.runemysteries.item.ItemRuneBag;
@@ -10,16 +12,24 @@ import com.laton95.runemysteries.spells.Spells;
 import com.laton95.runemysteries.spells.Spells.EnumSpell;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.play.server.SPacketCombatEvent.Event;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvent;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.Event.Result;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod.EventBusSubscriber
 public class ItemHelper {
+	private static Random random = new Random();
 	public static ItemStack getRuneBag(EntityPlayer player) {
 		for (int i = 0; i < player.inventory.getSizeInventory(); ++i) {
 			if (player.inventory.getStackInSlot(i).getItem() instanceof ItemRuneBag) {
@@ -31,7 +41,9 @@ public class ItemHelper {
 	
 	@SubscribeEvent
 	public static void pickupHandler(EntityItemPickupEvent event) {
+		
 		if (event.getItem().getItem().getItem() instanceof ItemRune) {
+			
 			ItemStack bag = getRuneBag(event.getEntityPlayer());
 			if (!bag.hasTagCompound()) {
 				bag.setTagCompound(new NBTTagCompound());
@@ -42,6 +54,8 @@ public class ItemHelper {
 			if (bag.getTagCompound().getBoolean("autoPickup")) {
 				InventoryRuneBag bagInventory = new InventoryRuneBag(bag);
 				event.getItem().getItem().setCount(bagInventory.mergeStack(event.getItem().getItem()));
+				event.getEntityPlayer().getEntityWorld().playSound((EntityPlayer) null, event.getEntityPlayer().posX, event.getEntityPlayer().posY, event.getEntityPlayer().posZ,
+						SoundEvents.ENTITY_ITEM_PICKUP, SoundCategory.PLAYERS, 0.2f, (random.nextFloat() - random.nextFloat()) * 1.4F + 2.0F);
 			}
 		}
 	}
